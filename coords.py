9d0059837546fa5fa7860f98bfc5396c3303c2a4
dasha <dashaman06@yandex.ru>
2022-03-07 14:52:07 +0300

    cearch

---
+++ coords.py
@@ -0,0 +1,18 @@
+def coords(x, y):
+    c = 0
+    d = 0
+    x = float(x)
+    y = float(y)
+    for i in range(17, -1, -1):
+        if x < 180 / 2 ** i:
+            d = i + 1
+            break
+    for j in range(17, -1, -1):
+        if y < 360 / 2 ** j:
+            c = j + 1
+            break
+    return (c + d) // 2
+
+
+p = coords(1.1645309999999967, 0.8790600000000026)
+print(p)
